b2392c5a38fb3cbcb81fcf11abde114bd48b983c
Juan Carlos Andreu <andreujuanc@gmail.com>
2019-05-13 22:35:46 +0200

    First commit

---
+++ OpenCqrs.Store.InMemory.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OpenCqrs.Store.InMemory.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenCqrs.Bus;
+using OpenCqrs.Dependencies;
+using OpenCqrs.Domain;
+using OpenCqrs.Extensions;
+using System;
+
+namespace OpenCqrs.Store.InMemory
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IOpenCqrsServiceBuilder AddInMemoryProvider(this IOpenCqrsServiceBuilder builder, IConfiguration configuration)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            var store = new InMemoryStore(new VersionService());// todo
+            var busdistpacher = new InMemoryBusMessageDispatcher();
+
+            builder.Services.AddSingleton<IEventStore>((sp) => store);
+            builder.Services.AddSingleton<ICommandStore>((sp) => store);
+            builder.Services.AddSingleton<IBusMessageDispatcher>((sp) => busdistpacher);
+            builder.Services.AddSingleton<IResolver, Resolver>();
+            
+
+            builder.Services.Scan(s => s
+                .FromAssembliesOf(typeof(InMemoryStore))
+                .AddClasses()
+                .AsImplementedInterfaces());
+
+            return builder;
+        }
+    }
+}
